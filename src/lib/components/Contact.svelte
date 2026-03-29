3d391a10ee8b8c270d9960a48216e3ef1f3602f4
Me <ryanh41137@gmail.com>
2026-03-29 14:29:23 -0700

    Update Scaling

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -4,7 +4,7 @@
 <div id="contact" class="h-screen bg-[#e5e5e5]">
     <!-- Spacer for nav bar -->
     <div class="h-3/16 md:h-1/9"></div>
-    <div class="flex items-center justify-center w-full px-4 rounded h-7/8">
-        <iframe class="rounded-md h-1/2 md:h-3/4 w-full" src={form} title="Contact Form"></iframe>
+    <div class="flex items-center justify-center w-full px-4 rounded h-15/16">
+        <iframe class="rounded-md h-full w-full" src={form} title="Contact Form"></iframe>
     </div>
 </div>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -5,7 +5,7 @@
 <div id="home" class="relative flex items-center justify-center h-screen bg-cover bg-no-repeat" style="background-image: url('{heroImg}');">
     <div>
         <h2 class="px-4 text-xl md:text-2xl lg:text-3xl text-white uppercase font-black">Join The</h2>
-        <h1 class="px-4 text-4xl md:text-5xl lg:text-7xl font-black text-shadow-lg text-shadow-white text-black uppercase">{title}</h1>
+        <h1 class="px-4 text-6xl lg:text-7xl font-black text-shadow-lg text-shadow-white text-black uppercase">{title}</h1>
     </div>
     <div class="absolute left-5 md:left-1/6 bottom-1/9">
         <a class="text-white text-xl md:text-2xl border font-bold uppercase bg-black p-3 rounded-md hover:cursor hover:underline hover:underline-offset-4" href="#contact">{cta}</a>
